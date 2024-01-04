4ebe7491f82e43d564b829cd2057f9dbda3b03ef
Achieve-software <vtcbcdtyr@gmail.com>
2024-01-04 07:58:47 +0300

    new url

--- src/hooks/useAuthCall.jsx
+++ src/hooks/useAuthCall.jsx
@@ -15,7 +15,7 @@ const useAuthCall = () => {
   const dispatch = useDispatch()
   const navigate = useNavigate()
 
-  const BASE_URL = "https://022179.fullstack.clarusway.com/"
+  const BASE_URL = "https://22179.fullstack.clarusway.com/"
 
   const login = async (userInfo) => {
     dispatch(fetchStart())
